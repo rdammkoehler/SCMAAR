3e31734c3ce21c0f627a2c0a512e7dbeebb687e5
rdammkoehler <rpd@noradltd.com>
2022-02-16 10:44:45 -0600

    python scaffold

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="SCMAAR",
+    version="0.0.0",
+    packages=[
+        "scmaar"
+    ],
+    install_requires=[
+    ],
+    extras_require={
+        "build": []
+    }
+)
